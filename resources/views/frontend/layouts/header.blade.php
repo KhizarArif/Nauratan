19077403725698e4cf0a96914380bfa65f3122f3
Mohammad khizar <m.khizar@ipscloud.co>
2025-07-24 16:57:55 +0500

    Working on Navbar Design

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -91,11 +91,11 @@
 
 <body>
     <!--Main Navigation-->
-    <header style="height: 15vh; background-color: #131212">
+    <header class="header-nav">
 
         <!-- Navbar -->
         <nav id="main-navbar" class="navbar navbar-expand-md fixed-top navbar-before-scroll shadow-0 "
-            style="height:13%;">
+            >
             <!-- Container wrapper -->
             <div class="container-fluid">
                 <!-- Toggle button -->
@@ -106,19 +106,20 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Collapsible wrapper -->
-                    <div class="navbar_container">
-                        <div class="row">
-                            <div class="col-md-2">
+                    <div class="">
+                        <!-- <div class="row"> -->
+                        <div class="d-flex justify-content-between align-items-center">
+                            <!-- <div class="col-md-2"> -->
                                 <div class="logo">
                                     <a href="{{ route('home') }}" class="navbar_brand m-1 ">
                                         <img src="{{ asset('frontend_assets/img/logo.jpg') }}" alt="logo"
                                             loading="lazy" class="navbar_brand_image" />
                                     </a>
                                 </div>
-                            </div>
+                            <!-- </div> -->
 
                             {{-- Categories Container Start  --}}
-                            <div class="col-md-8" style="margin: auto;">
+                            <!-- <div class="col-md-8" style="margin: auto;"> -->
                                 <div class="navbar_categories">
                                     <?php
 
@@ -127,24 +128,24 @@
                                     $subCategories = getSubCategories();
                                     ?>
                                     @if ($subCategories->isNotEmpty())
-                                        <ul class="navbar_subCategory">
-                                            @foreach ($subCategories as $getSubCategory)
-                                                <a href="javascript:void(0)" class="navbar_subCategory_link">
-                                                    <li class="navbar_subCategory_text">{{ $getSubCategory->name }}</li>
-                                                </a>
-                                            @endforeach
-                                        </ul>
+                                    <ul class="navbar_subCategory">
+                                        @foreach ($subCategories as $getSubCategory)
+                                        <a href="javascript:void(0)" class="navbar_subCategory_link">
+                                            <li class="navbar_subCategory_text">{{ $getSubCategory->name }}</li>
+                                        </a>
+                                        @endforeach
+                                    </ul>
                                     @endif
                                 </div>
 
-                            </div>
+                            <!-- </div> -->
                             {{-- Categories Container End   --}}
 
                             {{-- Social Container Start --}}
-                            <div class="col-md-2" style="margin: auto;">
+                            <!-- <div class="col-md-2" style="margin: auto;"> -->
                                 <ul class="social_media_container ">
-                                    <li class="nav-item cart_count_container"  >
-                                        <a class="nav-link pe-2 " href="{{ route('front.cart') }}" title="Add to cart" >
+                                    <li class="nav-item cart_count_container">
+                                        <a class="nav-link pe-2 " href="{{ route('front.cart') }}" title="Add to cart">
                                             <i class="fas fa-shopping-cart"></i>
                                             <div class="cart_count">{{ Cart::count() }}</div>
                                         </a>
@@ -173,67 +174,15 @@
                                         </li>
                                     </div>
                                 </ul>
-                            </div>
-                            {{-- Social Container End  --}}
+                            <!-- </div> -->
                         </div>
 
                     </div>
                 </div>
-                {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-                        <!-- Logo -->
-                        <a class="navbar-brand me-1" href="#"><img
-                                src="https://ascensus-mdb-uikit-tutorial.mdbgo.io/img/logo.png" height="20px"
-                                alt="Logo" loading="lazy" /></a>
-
-                        <li class="nav-item">
-                            <a class="nav-link text-dark" href="#!">Projects</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-dark" href="#!">About me</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-dark" href="#!">Testimonials</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-dark" href="#!">Contact</a>
-                        </li>
-                    </ul>
-
-                    <ul class="navbar-nav flex-row">
-                        <!-- Icons -->
-                        <li class="nav-item">
-                            <a class="nav-link pe-2" href="#!">
-                                <i class="fab fa-youtube"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-2" href="#!">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-2" href="#!">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link ps-2" href="#!">
-                                <i class="fab fa-instagram"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div> --}}
-                <!-- Collapsible wrapper -->
 
             </div>
-            <!-- Container wrapper -->
         </nav>
-        <!-- Navbar -->
 
 
 
     </header>
-    <!--Main Navigation-->
